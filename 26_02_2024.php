26dd46a0c1f2f9d9806a9560db54b818a778d028
Krane-13 <83793463+Krane-13@users.noreply.github.com>
2024-02-26 08:44:11 -0300

    Add files via upload

---
+++ 26_02_2024.php
@@ -0,0 +1,48 @@
+<?php
+	// $idade = 20;
+
+	// $passou_no_teste = false; //true -> aprovado falase -> reprovado
+
+	// if($idade >= 18 && $passou_no_teste == true){
+	// 	echo("Está apto a tirar a CNH");
+	// }
+	// else if($idade <18)
+	// 	echo("Para estar aptor, é <b>preciso ser maior de idade</b>");
+	// else
+	// 	echo("Para estar aptor, é <b>preciso passar no teste psicológico</b>");
+	// // else{
+	// // 	echo("<b>Não está apto</b> a tirar a CNH");
+	
+	//Atividade de escrever o mes por extenço 
+
+	$dia = 13;
+	$mes =12;
+	$ano = 2024;
+
+	if($mes == 1){
+		echo("$dia de JANEIRO de $ano");
+	}else if($mes == 2){
+		echo("$dia de FEVEREIRO de $ano");
+	}else if($mes == 3){
+		echo("$dia de MARÇO de $ano");
+	}else if($mes == 4){
+		echo("$dia de ABRIU de $ano");
+	}else if($mes == 5){
+		echo("$dia de MAIO de $ano");
+	}else if($mes == 6){
+		echo("$dia de JUNHO de $ano");
+	}else if($mes == 7){
+		echo("$dia de JULHO de $ano");
+	}else if($mes == 8){
+		echo("$dia de AGOSTO de $ano");
+	}else if($mes == 9){
+		echo("$dia de SETEMPRO de $ano");
+	}else if($mes == 10){
+		echo("$dia de OUTROBRO de $ano");
+	}else if($mes == 11){
+		echo("$dia de NOVEMBRO de $ano");
+	}else{
+		echo("$dia de DEZEMBRO de $ano");
+	}
+
+?>
